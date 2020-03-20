1906fcf1485890c9feba67b9bb5d955967bae838
Artem <a.lakiza.exceedteam@gmail.com>
2020-03-20 15:42:46 +0300

    Use CORS

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const todo = require("./routes/todo.route");
-const app = express();
+const cors = require('cors')
 const mongoose = require("mongoose");
+const app = express();
 const dev_db_url = "mongodb://localhost:27017/ProjectDB";
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
 const db = mongoose.connection;
 const port = 2000;
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/todos", todo);
